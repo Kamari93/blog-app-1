55abc683968a2c5c35d1b0a89e6bbcdbf53776f6
Kamari <kmoore971@gmail.com>
2025-04-15 15:22:39 -0500

    Add session expire functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,14 @@ import Post from "./Post";
 import EditPost from "./EditPost";
 import Contact from "./Contact";
 import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const userContext = createContext(); // create a global state
 
 function App() {
   const [user, setUser] = useState({});
   const [sessionExpired, setSessionExpired] = useState(false);
+  const navigate = useNavigate(); // for redirection
 
   axios.defaults.withCredentials = true;
 
@@ -38,35 +39,54 @@ function App() {
       });
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("https://blog-app-1-server.vercel.app/", { withCredentials: true })
-  //     .then((res) => {
-  //       if (res.data.username) {
-  //         setUser({ username: res.data.username });
-  //       } else {
-  //         setUser(null); // Set user to null if not logged in
-  //       }
-  //     })
-  //     .catch(() => setUser(null));
-  // }, []);
+  // Alert & Redirect if session expires for logged-in users
+  useEffect(() => {
+    if (sessionExpired && Object.keys(user).length > 0) {
+      alert("Your session has expired. Please log in again.");
+      setUser({});
+      navigate("/login");
+    }
+  }, [sessionExpired]);
+
+  // return (
+  //   <userContext.Provider value={{ user, setUser }}>
+  //     <BrowserRouter>
+  //       <Navbar />
+  //       <Routes>
+  //         <Route path="/" element={<Home />} />
+  //         <Route path="/register" element={<Register />} />
+  //         <Route path="/login" element={<Login />} />
+  //         <Route path="/create" element={<CreatePost />} />
+  //         <Route path="/post/:id" element={<Post />} />
+  //         <Route path="/editpost/:id" element={<EditPost />} />
+  //         <Route path="/contact" element={<Contact />} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </userContext.Provider>
+  // );
 
   return (
     <userContext.Provider value={{ user, setUser }}>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/create" element={<CreatePost />} />
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="/editpost/:id" element={<EditPost />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/editpost/:id" element={<EditPost />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </userContext.Provider>
   );
 }
 
-export default App;
+// export default App;
+
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
