fd2aba8ec64538b11601d3e7ebf34389fb3fdb84
Kamari <kmoore971@gmail.com>
2025-06-01 13:07:57 -0500

    Update expire functionality

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -30,6 +30,8 @@ function Navbar() {
         withCredentials: true,
       })
       .then((res) => {
+        localStorage.removeItem("token");
+        sessionStorage.removeItem("token");
         setUser({});
         setSessionExpired(true); // Mark session as expired/logged out
         navigate("/");
