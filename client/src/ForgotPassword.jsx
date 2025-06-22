f6aaf5d386a6caedc29a42e514ed7b57a4065022
Kamari <kmoore971@gmail.com>
2025-06-22 07:48:56 -0500

    Added styling to forgotpassword

--- client/src/ForgotPassword.jsx
+++ client/src/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -27,28 +28,39 @@ function ForgotPassword() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Forgot Password</h2>
-      <input
-        type="email"
-        placeholder="Email"
-        required
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Who is your favorite author?"
-        required
-        onChange={(e) => setSecurityAnswer(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="New Password"
-        required
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <button type="submit">Reset Password</button>
-      <div>{message}</div>
+    <form className="register_form" onSubmit={handleSubmit}>
+      <h2 className="register_title">Forgot Password</h2>
+      <div className="register_input_group">
+        <input
+          className="register_input"
+          type="email"
+          placeholder="Email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="register_input_group">
+        <input
+          className="register_input"
+          type="text"
+          placeholder="Who is your favorite author?"
+          required
+          onChange={(e) => setSecurityAnswer(e.target.value)}
+        />
+      </div>
+      <div className="register_input_group">
+        <input
+          className="register_input"
+          type="password"
+          placeholder="New Password"
+          required
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+      </div>
+      <button className="register_btn" type="submit">
+        Reset Password
+      </button>
+      <div className="register_message">{message}</div>
     </form>
   );
 }
